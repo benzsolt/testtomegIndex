662e492e589b5517c4cb271127caf43975acd6a5
benzsolt <57367149+benzsolt@users.noreply.github.com>
2019-11-11 11:28:33 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace testtomegIndex
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ez a program meghatározza a testtömeg-indexet.");
+            Console.Write("Kérem, adja meg a testsúlyát kg-ban: ");
+            double testSuly = double.Parse(Console.ReadLine());
+            Console.Write("Kérem, adja meg a magasságát méterben: ");
+            double testMagassag = double.Parse(Console.ReadLine());
+            double ttIndex = (testSuly / (testMagassag * testMagassag));
+            Console.WriteLine("Az Ön testtömeg-indexe: " + ttIndex);
+
+            double v = ttIndex;
+
+            if (v >= 20.5 && v <= 26.5)
+            {
+                Console.WriteLine("Gratulálunk! A testtömeg-indexe normál!");
+            }
+            else if (v >= 26.6 && v <= 31.9)
+            {
+                Console.WriteLine("Ön túlsúlyos!");
+            }
+            else if (v >= 32.0 && v <= 36.9)
+            {
+                Console.WriteLine("I. fokú elhízás!");
+            }
+            else if (v >= 37.0 && v <= 41.9)
+            {
+                Console.WriteLine("II. fokú elhízás!");
+            }
+            else
+            {
+                Console.WriteLine("III. fokú elhízás!");
+            }
+            Console.ReadKey();
+        }
+    }
+}
